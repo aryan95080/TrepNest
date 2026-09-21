8cc1e8bf740062127fa8e832ba9f526275c70cbc
amit kumar <amitarya95080@gmail.com>
2026-09-21 17:17:10 +0530

    Initait data

--- init/index.js
+++ init/index.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const TripNest = require("../models/tripnest.js");
-const initData= require("./data.js");
+const initData = require("./data.js");
 
 const MONGO_URI = "mongodb://127.0.0.1:27017/tripnest";
 
@@ -18,7 +18,7 @@ async function main() {
 
 const initDB = async () => {
   await TripNest.deleteMany({});
-  initData.data=initData.data.map((ele)=>({...ele,owner:"69a9d063f95297169d851d7f"}))
+  initData.data = initData.data.map((ele) => ({ ...ele, owner: "69a9d063f95297169d851d7f" }));
   await TripNest.insertMany(initData.data);
   console.log("Database initialized with sample data");
 };
